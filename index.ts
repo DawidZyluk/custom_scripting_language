e1958f13f51746d079e3519e789f4dca25a7649b
Dawid Zyluk <dawid@typoedition.com>
2023-04-17 11:46:38 +0200

    Change for...of to while; Add numbers, letters and space signs recognition

--- index.ts
+++ index.ts
@@ -5,8 +5,12 @@ enum TokenType {
   LeftParenthesis, 
   RightParenthesis,
   BinaryOperator,
-  Let,
+  Keyword,
 }
+
+const Keywords = [
+  "let",
+]
 
 const Operators = [
   "+",
@@ -19,26 +23,72 @@ class Token {
   value: string;
   type: TokenType;
 
-  constructor(value: string, type: TokenType) {
+  constructor(value = "", type: TokenType) {
     this.value = value;
     this.type = type;
   }
 }
 
-function tokenize(source: string): Token[] {
+function isAlpha(char: string) {
+	return char.toUpperCase() != char.toLowerCase();
+}
+
+function isSkippable(char: string) {
+	return char === " " || char === "\n" || char === "\t";
+}
+
+function isInt(char: string) {
+  return (char >= '0' && char <= '9');
+}
+
+function tokenize(sourceCode: string): Token[] {
   const tokens = new Array<Token>();
-  
-  for(let char of source) {
-    if(char === "(") {
-      tokens.push(new Token(char, TokenType.LeftParenthesis))
-    } else if (char === ")") {
-      tokens.push(new Token(char, TokenType.RightParenthesis))
-    } else if (Operators.includes(char)) {
-      tokens.push(new Token(char, TokenType.BinaryOperator))
-    } else if (char === "=") {
-      tokens.push(new Token(char, TokenType.Equals))
-    }
-  }
+	const src = sourceCode.split("");
+
+	while (src.length > 0) {
+		if (src[0] == "(") {
+			tokens.push(new Token(src.shift(), TokenType.LeftParenthesis));
+		} else if (src[0] == ")") {
+			tokens.push(new Token(src.shift(), TokenType.RightParenthesis));
+		} else if (Operators.includes(src[0])) {
+			tokens.push(new Token(src.shift(), TokenType.BinaryOperator));
+		} else if (src[0] == "=") {
+			tokens.push(new Token(src.shift(), TokenType.Equals));
+		} else {
+			if (isInt(src[0])) {
+				let num = "";
+				while (src.length > 0 && isInt(src[0])) {
+					num += src.shift();
+				}
+				tokens.push(new Token(num, TokenType.Number));
+			} else if (isAlpha(src[0])) {
+				let ident = "";
+				while (src.length > 0 && isAlpha(src[0])) {
+					ident += src.shift();
+				}
+
+				if (Keywords.includes(ident)) {
+					tokens.push(new Token(ident, TokenType.Keyword));
+				} else {
+					tokens.push(new Token(ident, TokenType.Identifier));
+				}
+			} else if (isSkippable(src[0])) {
+				src.shift();
+			} else {
+				console.error(
+					"Unreconized character found in source: ",
+					src[0].charCodeAt(0),
+					src[0]
+				);
+				Deno.exit(1);
+			}
+		}
+	}
 
   return tokens
+}
+
+const source = await Deno.readTextFile("./test.txt");
+for(const token of tokenize(source)) {
+  console.log({value: token.value, type: TokenType[token.type]})
 }
